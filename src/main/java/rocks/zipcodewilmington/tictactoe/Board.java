533db9436e0ec6d19ad6d0ae01fe1dad64a2d5ca
Eddie Morales <eddie@zipcoders-MacBook-Pro.local>
2019-10-26 13:38:08 -0400

    tictacToe

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -5,16 +5,26 @@ package rocks.zipcodewilmington.tictactoe;
  */
 public class Board {
     private Character[][] board;
-    public Board(Character[][] matrix) { this.board = matrix;
+
+    public Board(Character[][] matrix) {
+        this.board = matrix;
     }
-    public Boolean checkHorizontal(Character xO){
-        for(int i = 0; i <= 2; i++){
-            if((board[i][0] == xO) && (board[i][1] == xO) &&(board[i][2] == xO)){
+
+    public Boolean checkHorizontal(Character xO) {
+        for (int i = 0; i <= board.length; i++) {
+            Character firstRowFirstColumn = board[i][0];
+            Character firstRowSecondColumn = board[i][1];
+            Character firstRowThirdColumn = board[i][2];
+
+            if (firstRowFirstColumn == xO &&
+                    firstRowSecondColumn == xO &&
+                    firstRowThirdColumn == xO) {
                 return true;
             }
         }
         return false;
     }
+
     public Boolean checkVertical(Character xO) {
         for (int i = 0; i <= 2; i++) {
             if ((board[0][i] == xO) && (board[1][i] == xO) && (board[2][i] == xO)) {
@@ -23,18 +33,20 @@ public class Board {
         }
         return false;
     }
-    public Boolean checkDiagonal(Character xO){
+
+    public Boolean checkDiagonal(Character xO) {
         if ((board[0][0] == xO) && (board[1][1] == xO) && (board[2][2] == xO) ||
-                (board[2][0] == xO) && (board[1][1] == xO) && (board[0][2] == xO)){
+                (board[2][0] == xO) && (board[1][1] == xO) && (board[0][2] == xO)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-        ;
+
+    ;
 
     public Boolean isInFavorOfX() {
-        if (checkHorizontal ('X') || checkDiagonal('X') || checkVertical('X')){
+        if (checkHorizontal('X') || checkDiagonal('X') || checkVertical('X')) {
             return true;
         } else {
             return false;
@@ -43,27 +55,30 @@ public class Board {
     }
 
     public Boolean isInFavorOfO() {
-        if (checkHorizontal ('O') || checkDiagonal('O') || checkVertical('O')){
+        if (checkHorizontal('O') || checkDiagonal('O') || checkVertical('O')) {
             return true;
         } else {
             return false;
         }
 
     }
+
     public Boolean isTie() {
-        if(!isInFavorOfO() && !isInFavorOfX()){
+        if (!isInFavorOfO() && !isInFavorOfX()) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
     public String getWinner() {
-        if(isInFavorOfX()){
+        if (isInFavorOfX()) {
             return "X";
-        }if (isInFavorOfO()){
+        }
+        if (isInFavorOfO()) {
             return "O";
-        }return "";
+        }
+        return "";
     }
 
 }
